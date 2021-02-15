8e94e38e5b0cffa110153c482c17be78b9007a3b
Atul Kumar <mycodingx@gmail.com>
2021-02-15 20:39:32 +0530

    implementing socket on client side and handling socket cors error

--- client/src/components/Chat/index.jsx
+++ client/src/components/Chat/index.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+  const ENDPOINT = "localhost:5000";
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    socket = io(ENDPOINT);
+    setName(name);
+    setRoom(room);
+    console.log(socket);
+  }, [ENDPOINT, location.search]);
   return <div>Chat</div>;
 };
 
--- server/index.js
+++ server/index.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const sockertio = require("socket.io");
 const http = require("http");
+const cors = require("cors");
 
 const router = require("./router");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors());
+app.options("*", cors());
+
 const server = http.createServer(app);
-const io = sockertio(server);
+const io = sockertio(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 io.on("connection", (socket) => {
   console.log("We have a new socket connection..!!");
