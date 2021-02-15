ef3da069c1cd40ad748760b71166262a1050a811
Atul Kumar <mycodingx@gmail.com>
2021-02-15 20:19:51 +0530

    adding join component

--- client/src/components/Join/index.jsx
+++ client/src/components/Join/index.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Join.css";
 
 const Join = () => {
-  return <div>Join</div>;
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  return (
+    <React.Fragment>
+      <div className="joinOuterContainer">
+        <div className="joinInnerContainer">
+          <h1>Join</h1>
+          <div>
+            <input
+              type="text"
+              placeholder="Name"
+              className="joinInput"
+              onChange={(event) => setName(event.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Room"
+              className="joinInput mt-20"
+              onChange={(event) => setRoom(event.target.value)}
+            />
+            <Link
+              onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+              to={`/chat?name=${name}&room=${room}`}
+            >
+              <button className="button mt-20" type="submit">
+                Sign In
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Join;
--- server/index.js
+++ server/index.js
@@ -2,10 +2,21 @@ const express = require("express");
 const sockertio = require("socket.io");
 const http = require("http");
 
+const router = require("./router");
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
 const io = sockertio(server);
+
+io.on("connection", (socket) => {
+  console.log("We have a new socket connection..!!");
+  socket.on("disconnect", () => {
+    console.log("Connection off...!!");
+  });
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
